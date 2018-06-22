ba9be2107c2318f5d64a1a3b29c86b259bfbde81
Anthony Lee <anthonylee@umass.edu>
2018-06-22 16:45:33 -0400

    Project Started, Login Screen UI Updated

--- On the Map/LoginViewController.swift
+++ On the Map/LoginViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var userEmailTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
